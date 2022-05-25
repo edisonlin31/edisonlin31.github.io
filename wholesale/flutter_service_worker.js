691ff45579778d77be62b5734017f9236e43604a
Edison <edisonjnz31@gmail.com>
2022-05-25 20:22:47 +0700

    test

--- wholesale/flutter_service_worker.js
+++ wholesale/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "85d9c110d03dcdaa7238d150ba50e4cc",
-"index.html": "dba04fd7893277349a8e29c5145fcee0",
-"/": "dba04fd7893277349a8e29c5145fcee0",
-"main.dart.js": "709ffda0e6ee2faa70627c1e7908d9a9",
+"index.html": "213f1b360d57c998280d8fa2bf74f74b",
+"/": "213f1b360d57c998280d8fa2bf74f74b",
+"main.dart.js": "ff056a1865bd0065e594bafd78fa1d09",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "logo.png": "f4f61fa8405c27e1d011350a34a928a6",
 "icons/Icon-192.png": "86db90978e96849792a1bb70fe5ac3fa",
