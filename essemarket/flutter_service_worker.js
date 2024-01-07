0c3401bb049b95249616ae0c4aba04abefac5863
Edison <edisonjnz31@gmail.com>
2024-01-07 09:44:17 +0700

    update

--- essemarket/flutter_service_worker.js
+++ essemarket/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "5d38d55025f576fbdc2f8151da82d3fd",
-"index.html": "03a3252076cb27b4ff51946438749644",
-"/": "03a3252076cb27b4ff51946438749644",
-"main.dart.js": "94810ad65c1852aa140da609cd9c197f",
+"index.html": "ae177653f34c7efd714ed07c3ed44739",
+"/": "ae177653f34c7efd714ed07c3ed44739",
+"main.dart.js": "dc162a39b9d1b434d4ca573fd21a291b",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
